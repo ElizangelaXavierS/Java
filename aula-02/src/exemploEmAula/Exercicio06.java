5d453a433aae39ab1f73858eeb2701ea648014f8
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-02-10 13:31:53 -0300

    Exemplo feito em aula

---
+++ aula-02/src/exemploEmAula/Exercicio06.java
@@ -0,0 +1,17 @@
+package exemploEmAula;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+
+	public static void main(String[] args) {
+	Scanner leia = new Scanner(System.in);
+	
+	String nome;
+	int codigo;
+	float salario
+	
+
+	}
+
+}
---
+++ aula-03/src/tabuada/Tabuta.java
@@ -0,0 +1,41 @@
+package tabuada;
+
+import java.util.Scanner;
+
+public class Tabuta {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
+
+		int numero;
+		boolean ok = false;
+		String continua;
+
+		do {
+			do {
+				System.out.println("Digite a tabuada que voce deseja");
+				numero = leia.nextInt();
+
+				if (numero < 1 || numero > 10)
+					System.out.println("Digite um número entre 1 e 10");
+				else
+					ok = true;
+
+			} while (!ok);
+			int contador = 1;
+
+			while (contador <= 10) {
+				System.out.println("\n" + numero + " X " + contador + " = " + (numero * contador));
+				contador++;
+			}
+			ok = false;
+
+			System.out.println("Deseja continuar(S/N)?: ");
+			leia.skip("\\R?");
+			continua = leia.nextLine();
+
+		} while (continua.equals("S"));
+
+	}
+}
+
