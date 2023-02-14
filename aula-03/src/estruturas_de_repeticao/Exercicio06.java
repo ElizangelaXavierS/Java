be9ad235282506bc6d7eba2462578316029391fe
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-02-14 19:18:19 -0300

    Exercicio03

--- aula-03/src/estruturas_de_repeticao/Exercicio06.java
+++ aula-03/src/estruturas_de_repeticao/Exercicio06.java
@@ -6,25 +6,23 @@ public class Exercicio06 {
 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		
+
 		int numero, quantidade = 0;
 		float soma = 0, media;
-		
+
 		do {
-		System.out.println("Digite um número:");
-		numero = leia.nextInt();
-		
-		if (numero % 3 == 0) {
-			quantidade++;
-			soma += numero;	
-			
-			System.out.println("lllllllllllllll" + numero);
-			
-		}
-		
-		}while(numero != 0);
+			System.out.println("Digite um número:");
+			numero = leia.nextInt();
+
+			if (numero % 3 == 0) {
+				quantidade++;
+				soma += numero;
+
+			}
+
+		} while (numero != 0);
 		media = soma / quantidade;
-		
+
 		System.out.println("Amedia de todos os números multiplos de 3 é: " + media);
 		leia.close();
 
---
+++ aula-04/src/Vetores_Matrizes/Exercicio03.java
@@ -0,0 +1,27 @@
+package Vetores_Matrizes;
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+	public static void main(String[] args) {
+
+		int matrizInteiros[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		int cont = matrizInteiros.length - 1, somaPrincipal = 0, somaSecundaria = 0;
+		System.out.println("Elementos da diagonal principal:");
+		for(int i = 0; i < matrizInteiros.length; i++) {
+			System.out.print(matrizInteiros[i][i]+" ");
+			somaPrincipal += matrizInteiros[i][i];
+		}
+		
+		System.out.println("\n\nElementos da diagonal secundaria:");
+		for(int i = 0; i < matrizInteiros.length; i++) {
+			System.out.print(matrizInteiros[i][cont] + " ");
+			somaSecundaria += matrizInteiros[i][cont--];
+		}
+		System.out.println("\n\nSoma dos Elementos da diagonal principal: \n" + somaPrincipal);
+		System.out.println("\nSoma dos Elementos da diagonal secundario: \n" + somaPrincipal);
+
+	}
+
+}
