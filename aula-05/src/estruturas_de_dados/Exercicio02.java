1999057b564733258e4cc39316704d2abc1789b8
Elizangela Xavier <elizangelaxavier913@gmail.com>
2023-02-15 21:23:11 -0300

    Exercicio02

--- aula-05/src/estruturas_de_dados/Exercicio02.java
+++ aula-05/src/estruturas_de_dados/Exercicio02.java
@@ -1,7 +1,5 @@
 package estruturas_de_dados;
 
-
-
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -10,7 +8,7 @@ public class Exercicio02 {
 	public static void main(String[] args) {
 
 		Scanner leia = new Scanner(System.in);
-		Stack<String> fila = new Stack<String>();
+		Stack<String> pilha = new Stack<String>();
 
 		int opcao;
 		String nome;
@@ -18,10 +16,10 @@ public class Exercicio02 {
 		while (true) {
 			System.out.println("===========================================");
 			System.out.println("                                           ");
-			System.out.println("       1 - Adicionar cliene na fila        ");
-			System.out.println("       2 - Listar todos os clientes        ");
-			System.out.println("       3 - Retira o cliente da lista       ");
-			System.out.println("       4 - Sair                            ");
+			System.out.println("       1 - Adicionar livro na pilha        ");
+			System.out.println("       2 - Listar todos os livros          ");
+			System.out.println("       3 - Retira livro da pilha           ");
+			System.out.println("       0 - Sair                            ");
 			System.out.println("                                           ");
 			System.out.println("===========================================");
 			System.out.println("Entre com a opção desejada: ");
@@ -29,6 +27,42 @@ public class Exercicio02 {
 
 			opcao = leia.nextInt();
 
+			if (opcao == 0) {
+				System.out.println("O programa foi finalizado! ");
+				leia.close();
+				System.exit(0);
+
+			}
+			switch (opcao) {
+			case 1:
+				System.out.println("Adicionar livro!");
+				System.out.println("Digite o nome do livro: ");
+				leia.skip("\\R?");
+				nome = leia.nextLine();
+				pilha.add(nome);
+				;
+				System.out.println("Livro adicionado! ");
+
+				break;
+			case 2:
+				System.out.println("Consultar pilha de livros ");
+				pilha.forEach(System.out::println);
+
+				break;
+			case 3:
+				System.out.println("Retira Livro ");
+				if (pilha.isEmpty()) {
+					System.out.println("A pilha está vazia");
+				} else {
+					System.out.println("O Livro " + pilha.pop() + " foi retirado da pilha!");
+				}
+
+				break;
+			default:
+				System.out.println("Opção inválida! ");
+			}
+		}
+
 	}
 
-	}}
+}
